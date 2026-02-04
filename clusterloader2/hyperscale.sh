96e70a9cba5e4cffd0a87542713757ceeb14c232
vittoria salim <vslaim@microsoft.com>
2026-02-05 09:45:38 +1100

    testing

--- clusterloader2/hyperscale.sh
+++ clusterloader2/hyperscale.sh
@@ -1,6 +1,6 @@
 go run ./cmd/clusterloader.go --provider aks -v=4 \
     --testconfig ./testing/load/config.yaml \
-    --kubeconfig $HOME/cl2-debug \
+    --kubeconfig $HOME/hs-64 \
     --enable-prometheus-server=true \
     --tear-down-prometheus-server=true \
     --testoverrides=/home/vsalim/perf-tests/clusterloader2/testing/overrides/hyperscale.yaml \
